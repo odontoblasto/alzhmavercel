da8629137635d2fdf65f5c03b4bd88314842561e
The MH Team <cedfv@cin.ufpe.br>
2023-04-26 07:09:55 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 // const { auth } = require('../frontend/src/config/firebase') 
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 require('dotenv').config();
 
 const app = express();
@@ -10,13 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 
-const uri = process.env.ATLAS_URI;
+//const uri = process.env.ATLAS_URI;
 //const uri =" mongodb+srv://cedfv:Rosinhaalzhma@alzhma.xcomflh.mongodb.net/?retryWrites=true&w=majority"
-mongoose.connect(uri);
-const connection = mongoose.connection;
-connection.once('open', () => {
-console.log("MongoDB database connection established successfully");
-})
+//mongoose.connect(uri);
+//const connection = mongoose.connection;
+//connection.once('open', () => {
+//console.log("MongoDB database connection established successfully");
+//})
 
 // const uid = auth.currentUser.uid
 const postSchema = mongoose.Schema({
